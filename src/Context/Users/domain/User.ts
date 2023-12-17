08c737a36dc7699b6438fe0d9d65fd599e9eedc6
bpa03 <baldassarepugliese@gmail.com>
2023-12-17 17:22:22 -0400

    chore: Updating some types for user entity

--- src/Context/Users/domain/User.ts
+++ src/Context/Users/domain/User.ts
@@ -32,7 +32,7 @@ export class User extends AggregateRoot {
     };
   }
 
-  static fromPrimitives ({ email, id, lastname, name, gender, password }: { id: string, email: string, name?: string, lastname?: string, gender?: string, password: string }): User {
+  static fromPrimitives ({ email, id, lastname, name, gender, password }: { id: string, email: string, name?: string | null, lastname?: string | null, gender?: string | null, password: string }): User {
     return new User({
       id: new UserId(id),
       email: new UserEmail(email),
