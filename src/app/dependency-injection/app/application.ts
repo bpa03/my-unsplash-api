077f258140632b60521c043e9f447b03ed041975
bpa03 <baldassarepugliese@gmail.com>
2023-12-18 15:25:03 -0400

    chore: Creating middleware to validate req schema and updating dependencies

--- src/app/dependency-injection/app/application.ts
+++ src/app/dependency-injection/app/application.ts
@@ -1,7 +1,13 @@
-import { type ContainerBuilder } from 'node-dependency-injection';
+import { Reference, type ContainerBuilder } from 'node-dependency-injection';
 import { StatusGetController } from '../../controllers/StatusGetController';
+import { UserRegistrarPostController } from '../../controllers/UserRegisterPostController';
+import { ValidateReqSchemaMiddleware } from '../../middlewares/ValidateReqSchemaMiddleware';
 
 export const registerAppControllers = (container: ContainerBuilder): void => {
+  // Middlewares
+  container.register('App.Backend.middlewares.ValidateReqSchemaMiddleware', ValidateReqSchemaMiddleware);
+
   // Controllers
   container.register('App.Backend.controllers.StatusGetController', StatusGetController);
+  container.register('App.Backend.controllers.UserRegistrarPostController', UserRegistrarPostController).addArgument(new Reference('App.User.Application.UserRegistrar'));
 };
